160816314326588b7f977d096f0765facd55ff3d
Hameed Khan <hameed404404@gmail.com>
2026-05-22 17:24:12 +0500

    Added Singleton Pattern

---
+++ 07_SingletonDesignPattern/SimpleSingleton.java
@@ -0,0 +1,15 @@
+public class SimpleSingleton{
+    private static SimpleSingleton instance = null;
+    public static SimpleSingleton getInstance() {
+        if (instance == null) {
+            instance = new SimpleSingleton();
+        }
+        return instance;
+    }
+
+    public static void main(String[] args) {
+        SimpleSingleton s1 = SimpleSingleton.getInstance();
+        SimpleSingleton s2 = SimpleSingleton.getInstance();
+        System.out.println(s1 == s2);
+    }
+}
